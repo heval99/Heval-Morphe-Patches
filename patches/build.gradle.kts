e7ec9b56a1aea3f92866dc9c0739115b53dc2019
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-01-24 09:04:36 +0100

    chore: Add `PatchListGenerator` (#6)

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -17,3 +17,29 @@ kotlin {
         freeCompilerArgs.add("-Xcontext-parameters")
     }
 }
+
+dependencies {
+    // Used by JsonGenerator.
+    implementation(libs.gson)
+}
+
+tasks {
+    register<JavaExec>("generatePatchesList") {
+        description = "Build patch with patch list"
+
+        dependsOn(build)
+
+        classpath = sourceSets["main"].runtimeClasspath
+        mainClass.set("app.morphe.util.PatchListGeneratorKt")
+    }
+    // Used by gradle-semantic-release-plugin.
+    publish {
+        dependsOn("generatePatchesList")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs = listOf("-Xcontext-receivers")
+    }
+}
