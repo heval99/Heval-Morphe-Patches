d404143076665a851249065c4c8f168b8104eccf
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-05-20 19:54:29 +0200

    chore: Update header to the current repo

--- patches/src/main/kotlin/util/PatchListGenerator.kt
+++ patches/src/main/kotlin/util/PatchListGenerator.kt
@@ -1,34 +1,6 @@
 /*
  * Copyright 2025 Morphe.
- * https://github.com/MorpheApp/morphe-patches
- *
- * File-Specific License Notice (GPLv3 Section 7 Terms)
- *
- * This file is part of the Morphe patches project and is licensed under
- * the GNU General Public License version 3 (GPLv3), with the Additional
- * Terms under Section 7 described in the Morphe patches
- * LICENSE file: https://github.com/MorpheApp/morphe-patches/blob/main/NOTICE
- *
- * https://www.gnu.org/licenses/gpl-3.0.html
- *
- * File-Specific Exception to Section 7b:
- * -------------------------------------
- * Section 7b (Attribution Requirement) of the Morphe patches LICENSE
- * does not apply to THIS FILE. Use of this file does NOT require any
- * user-facing, in-application, or UI-visible attribution.
- *
- * For this file only, attribution under Section 7b is satisfied by
- * retaining this comment block in the source code of this file.
- *
- * Distribution and Derivative Works:
- * ----------------------------------
- * This comment block MUST be preserved in all copies, distributions,
- * and derivative works of this file, whether in source or modified
- * form.
- *
- * All other terms of the Morphe Patches LICENSE, including Section 7c
- * (Project Name Restriction) and the GPLv3 itself, remain fully
- * applicable to this file.
+ * https://github.com/MorpheApp/morphe-patches-template
  */
 
 package util
